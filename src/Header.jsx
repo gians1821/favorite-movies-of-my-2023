01f80cc183a44bdf9a213b51431315b0f0744f74
gians1821 <gian.samana.1801@gmail.com>
2024-01-18 10:21:30 -0500

    feat: changed styles for a text align left

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,7 +3,7 @@ import './Header.css'
 function Header() {
     return (
         <header className='header'>
-            <h2 className='header-title'>He visto 101 películas y series en 2023</h2>
+            <h2 className='header-title'><span className='header-higlight'>He visto</span><span className='header-number'>101</span> películas y series <span className='header-year'>en 2023</span></h2>
             <p className='header-description'>Estas son mis favoritas...</p>
         </header>
     );
